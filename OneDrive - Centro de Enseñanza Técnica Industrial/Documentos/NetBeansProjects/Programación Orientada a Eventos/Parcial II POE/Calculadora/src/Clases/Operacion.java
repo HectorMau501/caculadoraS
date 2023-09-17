ddb2783bba9ad2e798d34407b69a855d4c445fe4
HectorMau501 <mauhector7@gmail.com>
2023-09-16 23:37:07 -0500

    Termino del programa de la calculadora con hilos

---
+++ OneDrive - Centro de Enseñanza Técnica Industrial/Documentos/NetBeansProjects/Programación Orientada a Eventos/Parcial II POE/Calculadora/src/Clases/Operacion.java
@@ -0,0 +1,12 @@
+
+package Clases;
+
+public interface Operacion {
+    public double suma(double a, double b);
+    public double resta(double c, double d);
+    public double div(double e, double f);
+    public double multiplicacion(double g, double h);
+    public double modulo(double i, double j);
+    public double raiz(double e, double f);
+    public double pow(double k, double l);
+}
---
+++ OneDrive - Centro de Enseñanza Técnica Industrial/Documentos/NetBeansProjects/Programación Orientada a Eventos/Parcial II POE/Calculadora/src/Clases/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author HECTOR MAURICIO
+ */
+public class main {
+    public static void main(String[] args) {
+       interfaces objeto1 = new interfaces();
+        objeto1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //cerrar la ventana
+    }
+}
